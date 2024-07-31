2fb0a92a3ba2bf87438cdc96a9510ddec7bddd3b
幻梦颜 <anyh2210259@outlook.com>
2024-07-31 10:07:36 +0800

    Add files via upload

--- 刮刮乐/刮刮乐/再来一次.gd
+++ 刮刮乐/刮刮乐/再来一次.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 func 开始():
 	visible=0
-	GGL.print提示("%d -消耗100单位 = %d"%[GGL.单位,GGL.单位-100],Color(1,0,0,1))
+	GGL.print提示("%d -消耗100单位 = %d"%[GGL.单位,GGL.单位-100])
 	GGL.单位-=100
 
 	GGL.改变.emit()
@@ -21,4 +21,4 @@ func _on_button_up() -> void:
 	if GGL.单位>=100:
 		GGL.开始.emit()
 	else:
-		GGL.print提示("你没钱了",Color(1,0,0,1))
+		GGL.print提示("你没钱了")
--- 刮刮乐/刮刮乐/自动加载.gd
+++ 刮刮乐/刮刮乐/自动加载.gd
@@ -10,24 +10,24 @@ var 没有中奖:int
 var 提示文本框=preload("res://刮刮乐/提示文本框.tscn").instantiate()
 const 提示文本 = preload("res://刮刮乐/提示文本.tscn")
 
+
+
+
+
 func _ready() -> void:
 	中奖.connect(HS中奖.bind())
 	add_child(提示文本框)
 	
 func HS中奖():
 	if 没有中奖>=25:
-		GGL.print提示("没有中奖",Color(1,0,0,1))
+		GGL.print提示("没有中奖")
 
-func print提示(文本,颜色=Color(0,1,0,1)):
+func print提示(文本,颜色="#FF0000"):
 	var 提示=提示文本.instantiate()
 	提示.text=str(文本)
 	提示.set_modulate(颜色)
 	add_child(提示)
-	var color
-	if 颜色==Color(1,0,0,1):
-		color="red"
-	elif 颜色==Color(0,1,0,1):
-		color="green"
-	提示文本框.append_text("[color=%s]%s\n"%[color,文本])
+
+	提示文本框.append_text("[color=%s]%s\n"%[颜色,文本])
 
 
